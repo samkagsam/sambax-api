90eb18b5466716ec16aaed27924ce47e05e1f066
samkagsam <samuelkagamba52@gmail.com>
2022-12-29 14:56:55 +0000

    hide twilio credentials

--- app/config.py
+++ app/config.py
@@ -12,6 +12,8 @@ class Settings(BaseSettings):
     access_token_expire_minutes: int
     box_uganda_username: str
     box_uganda_password: str
+    twilio_account_sid: str
+    twilio_auth_token: str
 
     class Config:
         env_file =".env"
--- app/testwilio.py
+++ app/testwilio.py
@@ -1,15 +1,17 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 import os
+from .config import settings
 from twilio.rest import Client
+
 
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
-account_sid = "ACf3b2717f63d35e0b35e770fefccd9faf"
-auth_token = "bc2487d1653d69076c8c49e02408c2bc"
+account_sid = settings.twilio_account_sid
+auth_token = settings.twilio_auth_token
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-  body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+  body="Join Earth's mightiest heroes. Like yourself.",
   from_="+13344713159",
   to="+256705579354"
 )
